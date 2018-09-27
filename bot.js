c29d7b8181c693faa0f54b34dcc63116cc5aeca5
looz22 <43042896+looz22@users.noreply.github.com>
2018-09-27 19:12:11 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '!'
+const prefix = '2'
+
 
 client.on('ready', () => {
-  client.user.setActivity("!help |by looz ",{type: 'PLAYING'})
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+   console.log(----------------);
+      console.log(Desert Bot- Script By : tx1 music 2#8091 );
+        console.log(----------------);
+      console.log(ON ${client.guilds.size} Servers '     Script By : tx1 music 2#8091  ' );
+    console.log(----------------);
+  console.log(Logged in as ${client.user.tag}!);
+client.user.setGame(Tx1 Network.,"http://twitch.tv/S-F%22)
+client.user.setStatus("dnd")
 });
-
 
 
 
